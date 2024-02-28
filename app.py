f956ea30626786f47647d180f4ae8033a560a6b0
TnTeQAQ <103574636+TnTeQAQ@users.noreply.github.com>
2024-02-28 08:21:09 +0800

    设置FLASK_APP

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
 app.config['GEMINI_API_KEY'] = config.GEMINI_API_KEY
 
+os.environ['FLASK_APP'] = 'app.py'
+
 app.register_blueprint(website, url_prefix='/')
 app.register_blueprint(backend, url_prefix='/model')
 
